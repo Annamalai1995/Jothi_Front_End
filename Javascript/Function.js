740aabeeb04808670bdc0720f1c436c6885b833a
Annamalai1995 <s.annamalai1995@gmail.com>
2024-11-08 10:14:45 +0530

    bootstarp over

---
+++ Javascript/Function.js
@@ -0,0 +1,17 @@
+function cosmo()
+{
+    alert("tHIS is normal function in javascript=")
+
+}
+function beta(studentname,college)
+{
+    alert("StudentName is"+studentname+":Collegename is"+college)
+}
+var alpha=()=>
+{
+    document.writeln("This is Fat Arrow function")
+}
+const Add=(a,b)=>
+{
+return a+b;
+}
